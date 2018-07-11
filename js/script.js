a8efee7b55ed61491beafd95a9a710be29e4ddf8
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-07-12 03:01:29 +0600

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+(function ($) {
+    'use strict';
+    
+    // -----------------------------
+    //  Headroom Initialize
+    // -----------------------------
+    $('.main-nav').headroom();
+    
+    // -----------------------------
+    //  AOS Initialize
+    // -----------------------------
+    AOS.init();
+
+    // Smooth Scroll to Section
+    $('a.page-scroll').on('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 60)
+        }, 700);
+        event.preventDefault();
+    });
+
+    $(window).on('scroll', function () {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('.main-nav').addClass('nav-bg');
+        } else {
+            $('.main-nav').removeClass('nav-bg');
+        }
+    });
+
+})(jQuery);
