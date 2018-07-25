cefe98686b7deab304a5dd19f78de8d35bb7a0b0
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-07-26 00:03:44 +0600

    Added Animation On Steps & Removed Preloader

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     'use strict';
-    
-    PageLoad();
     smoothScroll()
     // -----------------------------
     //  Headroom Initialize
@@ -56,56 +54,6 @@ $(window).on('scroll', function () {
         $('.main-nav').removeClass('nav-bg');
     }
 });
-
-/*--------------------------------------------------
-Function Page Load
----------------------------------------------------*/
-
-function PageLoad() {
-
-    $('body').removeClass('hidden');
-    TweenMax.to($('.preloader-text'), 1, { force3D: true, opacity: 1, y: 0, delay: 0.2, ease: Power3.easeOut });
-
-    var width = 100,
-        perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
-        EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-        time = parseInt((EstimatedTime / 500) % 50) * 70;
-
-
-    // Percentage Increment Animation
-    var PercentageID = $('#precent'),
-        start = 0,
-        end = 100,
-        durataion = time;
-    animateValue(PercentageID, start, end, durataion);
-
-    function animateValue(id, start, end, duration) {
-
-        var range = end - start,
-            current = start,
-            increment = end > start ? 1 : -1,
-            stepTime = Math.abs(Math.floor(duration / range)),
-            obj = $(id);
-
-        var timer = setInterval(function () {
-            current += increment;
-            $(obj).text(current);
-            //obj.innerHTML = current;
-            if (current == end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
-    }
-
-    // Fading Out Loadbar on Finised
-    setTimeout(function () {
-        TweenMax.to($('.percentage, .preloader-text'), 0.7, { force3D: true, opacity: 0, yPercent: -101, ease: Power3.easeInOut });
-        TweenMax.to($('.preloader-wrap'), 0.7, { force3D: true, yPercent: -101, delay: 0.2, ease: Power3.easeInOut });
-    }, time);
-
-
-
-}// End Page Load	
 
 // -----------------------------
 // Smmoth Scroll
